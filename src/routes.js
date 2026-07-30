5c069d89e4fea6dd754f94cc98168e19b6bb5cc9
charsle3 <jpkingthe3rd@gmail.com>
2026-07-30 15:55:03 -0700

    registering new users

--- src/routes.js
+++ src/routes.js
@@ -31,6 +31,7 @@ import {
     processEditCategoryForm
     } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
+import { showUserRegistrationForm, processUserRegistrationForm } from './controllers/users.js';
 
 const router = express.Router();
 
@@ -79,6 +80,10 @@ router.post('/new-category', categoryValidation, processNewCategoryForm);
 // Routes for editing categories
 router.get('/edit-category/:id', showEditCategoryForm);
 router.post('/edit-category/:id', categoryValidation, processEditCategoryForm);
+
+// User registration routes
+router.get('/register', showUserRegistrationForm);
+router.post('/register', processUserRegistrationForm);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
